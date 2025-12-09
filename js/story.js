d3f261c81412be45c966447de76c057df9e46353
Anum <127afarghani@frhsd.com>
2025-12-09 12:50:24 -0500

    meow

--- js/story.js
+++ js/story.js
@@ -1,1 +1,57 @@
 alert ('yay! story time whoo hoo!')
+
+var nameBox = document.getElementById("name");
+var adjectiveOne = document.getElementById("adjective");
+var clothIng = document.getElementById("clothing");
+var objectOne = document.getElementById("object");
+var adjectiveTwo = document.getElementsByName("adjective2");
+var veHicle = document.getElementsByClassName("vehicle");
+var plAce = document.getElementById("place");
+var objectTwo = document.getElementById("object2");
+var superPower = document.getElementById("superpower");
+var objectTwo = document.getElementById("object2");
+var nickName = document.getElementById("nickname");
+var adjectiveThree = document.getElementById("adjective3");
+
+console.log(nameBox);
+console.log(adjectiveOne);
+console.log(clothIng);
+console.log(objectOne);
+console.log(adjectiveTwo);
+console.log(veHicle);
+console.log(plAce);
+console.log(objectTwo);
+console.log(nickName);
+console.log(adjectiveThree);
+
+
+var checkBoxes = document.querySelectorAll('input[type="checkbox"]');
+// console.log(checkBoxes);
+
+
+butTon.addEventListener ('click', myFunction);
+function myFunction() {
+    const nameBox = userName.value;
+    const adjectiveOne = adjective.value;
+    /*
+    const genderBox = genderInput.value;
+    const userName = nameInput.value;
+    const ageUser = ageInput.value;
+    const genderBox = genderInput.value;
+    */
+    const selectedValue = [];
+    for(let i = 0; i < jobBox.length; i++ ) {
+        if (jobBox[i].checked) {
+            selectedValue.push(jobBox[1].value)
+        }
+        const checkedValues = [];
+        checkBoxes.forEach(checkbox => {
+            if (checkbox.checked) {
+                checkedValues.push(checkbox.value);
+            }
+        });
+
+        const genderUser = genderInput.options[genderInput.selectedIndex].text;
+        outPut.innerHTML = ('Full Name:') + (' ') + (userName) + ('<br>') + ('Age:') + (' ') + (ageUser) + ('<br>') + ('Where did you attend college?') + (' ') + (selectedValue) + ('<br>') + ('Gender:') + (' ' ) + (genderUser) + ('<br>') + ('Skills:') + (' ') +  (checkedValues);
+    }
+}
